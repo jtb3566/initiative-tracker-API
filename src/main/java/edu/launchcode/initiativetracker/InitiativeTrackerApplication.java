4b969f8e0d781ceb34e46777e0b840147ba6c307
James Ballard <ballardj77@gmail.com>
2022-03-23 13:00:12 -0500

    initial commit for spring boot aplication

---
+++ src/main/java/edu/launchcode/initiativetracker/InitiativeTrackerApplication.java
@@ -0,0 +1,13 @@
+package edu.launchcode.initiativetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitiativeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitiativeTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/launchcode/initiativetracker/InitiativeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.launchcode.initiativetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitiativeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
