77a7dfeacb0a0843142e787e70d31579820fdf1e
James Ballard <ballardj77@gmail.com>
2022-04-01 19:02:22 -0500

    added delete and getAll paths to createdCharacter controller

--- src/main/java/edu/launchcode/initiativetracker/controller/CreatedCharacterController.java
+++ src/main/java/edu/launchcode/initiativetracker/controller/CreatedCharacterController.java
@@ -1,8 +1,12 @@
 package edu.launchcode.initiativetracker.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +26,19 @@ public class CreatedCharacterController {
     public ResponseEntity<CreatedCharacter> createCharacter(@RequestBody CreatedCharacter createdCharacter) {
         CreatedCharacter newCharacter = createdCharacterRespository.save(createdCharacter);
         return ResponseEntity.ok().body(newCharacter);
+    }
+
+    @GetMapping(value="/getAll")
+    public ResponseEntity<List<CreatedCharacter>> returnAllEncounters () {
+        List<CreatedCharacter> createdCharacters = new ArrayList<>();
+        createdCharacterRespository.findAll().forEach(createdCharacters::add);
+        return ResponseEntity.ok().body(createdCharacters);
+    }
+
+    @PostMapping(value="/delete", consumes="application/json")
+    public ResponseEntity<Boolean> deleteEncounter (@RequestBody CreatedCharacter createdCharacter) {
+        createdCharacterRespository.delete(createdCharacter);
+        Boolean deleted = !(createdCharacterRespository.existsById(createdCharacter.getId()));
+        return ResponseEntity.ok().body(deleted);
     }
 }
